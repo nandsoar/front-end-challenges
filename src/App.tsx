84601a251c86006730961893eb3e868ff0a35902
Keven <hkeven89@gmail.com>
2025-12-27 21:05:51 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import C25 from './components/QR-Code'
+// import HolyGrail from './components/HolyGrail'
+// import CenterBlock from './components/CenterBlock'
+// import OneGrid from './components/OneGrid'
+
+function App() {
+  return (
+    <div className="app">
+      {/*<OneGrid />*/}
+      {/*<HolyGrail />*/}
+      <C25 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/QR-Code.tsx
@@ -0,0 +1,15 @@
+import styles from '@/styles/QR-Code.module.css'
+
+export default function C25() {
+  return (
+    <div className={styles.root}>
+      <article>
+        <img src="qrimage.svg" alt="qr-image" />
+        <div className={styles.about}>
+          <h1>Improve your front-end skills by building projects</h1>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </article>
+    </div>
+  )
+}
